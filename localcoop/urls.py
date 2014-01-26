5b3fd0c6fb7d6a21a06ec755a617582f5b6111ce
hedgehogrider <gwelkind@gmail.com>
2014-01-25 21:32:39 -0500

    Guess I never added urls.py

--- localcoop/urls.py
+++ localcoop/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +8,14 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('subscriptions.views',
+    url(r'^register/','register_coopuser_view'),
+    url(r'^sorry/','sorry_past_deadline_view'),
+    url(r'^process-registration/','process_registration'),
+    url(r'^thanks-for-registering/','thanks_for_registering'),
+    url(r'^login/','login'),
+    url(r'^login-successful', 'login_successful'),
+    url(r'^user/(?P<username_string>\w+)/$','user_page'),
 )
